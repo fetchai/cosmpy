8ec5bccdde4f99bb16c99dd3a2077a224307dc73
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2021-06-02 00:27:28 +0100

    feat: add address support

--- src/cosm/crypto/address.py
+++ src/cosm/crypto/address.py
@@ -1,3 +1,44 @@
+from typing import Union, Optional
+
+import bech32
+from cosm.crypto.hashfuncs import sha256, ripemd160
+from cosm.crypto.keypairs import PublicKey
+
+DEFAULT_PREFIX = 'fetch'
+
+
+def _to_bech32(prefix: str, data: bytes) -> str:
+    data_base5 = bech32.convertbits(data, 8, 5, True)
+    return bech32.bech32_encode(prefix, data_base5)
+
+
 class Address:
-    def __init__(self, address: Union[str, bytes, 'Address', PublicKey]):
-        pass
+    def __init__(self, value: Union[str, bytes, PublicKey, 'Address'], prefix: Optional[str] = None):
+        if isinstance(value, str):
+            _, data_base5 = bech32.bech32_decode(value)
+            if data_base5 is None:
+                raise RuntimeError('Unable to parse address')
+
+            self._address = bytes(bech32.convertbits(data_base5, 5, 8, False))
+            self._display = value
+
+        elif isinstance(value, bytes):
+            if len(value) != 20:
+                raise RuntimeError('Incorrect address length')
+
+            self._address = value
+            self._display = _to_bech32(prefix or DEFAULT_PREFIX, self._address)
+
+        elif isinstance(value, PublicKey):
+            self._address = ripemd160(sha256(value.public_key_bytes))
+            self._display = _to_bech32(prefix or DEFAULT_PREFIX, self._address)
+
+        elif isinstance(value, Address):
+            self._address = value._address
+            self._display = value._display
+
+    def __str__(self):
+        return self._display
+
+    def __bytes__(self):
+        return self._address
---
+++ src/cosm/crypto/hashfuncs.py
@@ -0,0 +1,16 @@
+import hashlib
+
+
+def sha256(contents: bytes) -> bytes:
+    h = hashlib.sha256()
+    h.update(contents)
+    return h.digest()
+
+
+def ripemd160(contents: bytes) -> bytes:
+    if 'ripemd160' not in hashlib.algorithms_available:
+        raise RuntimeError('ripemd160 hash not supported on your platform')
+
+    h = hashlib.new('ripemd160')
+    h.update(contents)
+    return h.digest()
