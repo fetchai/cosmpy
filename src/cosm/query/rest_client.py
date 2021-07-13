ab11b177a20be8f12ae04048a85cb721919320c4
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2021-07-13 16:58:04 +0100

    HTTP client (#5)

---
+++ src/cosm/query/rest_client.py
@@ -0,0 +1,14 @@
+import requests
+from common import JSONLike
+
+
+class RestClient:
+    def __init__(self, rest_address: str):
+        self._session = requests.session()
+        self.rest_address = rest_address
+
+    def query(self, request: str) -> JSONLike:
+        response = self._session.get(url=self.rest_address + request)
+        if response.status_code != 200:
+            raise RuntimeError(f"Error when sending a query request.\n Request: {request}\n Response: {response})")
+        return response.json()
