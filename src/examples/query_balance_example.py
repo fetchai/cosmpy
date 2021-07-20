0877b86c763d2152a080a1ad1f8e013bf4493940
jiri <jiri.vestfal@fetch.ai>
2021-07-20 15:29:19 +0100

    script changes

--- src/examples/query_balance_example.py
+++ src/examples/query_balance_example.py
@@ -1,8 +1,9 @@
 from cosm.bank.rest_client import BankRestClient, QueryBalanceRequest
 
-auth = BankRestClient("http://127.0.0.1:1317")
+rest_url = "http://127.0.0.1:1317"
 address = "fetch1mrf5yyjnnlpy0egvpk2pvjdk9667j2gtu8kpfy"
 denom = "stake"
 
+auth = BankRestClient(rest_url)
 res = auth.Balance(QueryBalanceRequest(address=address, denom=denom))
 print(f"Balance of {address} is {res.balance.amount} {res.balance.denom}")
