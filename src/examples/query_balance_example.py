1a3da70568d24c718a23cc7d79e7f36d00300bec
jiri <jiri.vestfal@fetch.ai>
2021-07-23 09:43:04 +0100

    typo fix

--- src/examples/query_balance_example.py
+++ src/examples/query_balance_example.py
@@ -4,6 +4,6 @@ rest_url = "http://127.0.0.1:1317"
 address = "fetch1mrf5yyjnnlpy0egvpk2pvjdk9667j2gtu8kpfy"
 denom = "stake"
 
-auth = BankRestClient(rest_url)
-res = auth.Balance(QueryBalanceRequest(address=address, denom=denom))
+bank = BankRestClient(rest_url)
+res = bank.Balance(QueryBalanceRequest(address=address, denom=denom))
 print(f"Balance of {address} is {res.balance.amount} {res.balance.denom}")
