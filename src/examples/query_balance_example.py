d4ad1b92af52d92660ae7ab3931a19474c22b4a5
S Ali Hosseini <38721653+5A11@users.noreply.github.com>
2021-08-04 17:31:11 +0100

    fix: various fixes to examples (#29)

--- src/examples/query_balance_example.py
+++ src/examples/query_balance_example.py
@@ -1,9 +1,10 @@
 from cosm.bank.rest_client import BankRestClient, QueryBalanceRequest
+from cosm.query.rest_client import QueryRestClient
 
 REST_URL = "http://127.0.0.1:1317"
 ADDRESS = "fetch1mrf5yyjnnlpy0egvpk2pvjdk9667j2gtu8kpfy"
 DENOM = "stake"
 
-bank = BankRestClient(REST_URL)
+bank = BankRestClient(QueryRestClient(REST_URL))
 res = bank.Balance(QueryBalanceRequest(address=ADDRESS, denom=DENOM))
 print(f"Balance of {ADDRESS} is {res.balance.amount} {res.balance.denom}")
