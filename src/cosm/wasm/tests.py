8ecd26925699d9169649ac0e4501c368899113e8
jiri <jiri.vestfal@fetch.ai>
2021-08-04 15:42:20 +0100

    rest of tests

--- src/cosm/wasm/tests.py
+++ src/cosm/wasm/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from cosm.wasm.rest_client import WasmRestClient
 from cosm.tests.helpers import MockQueryRestClient
+import base64
 
 from cosmwasm.wasm.v1beta1.query_pb2 import (
     QuerySmartContractStateRequest,
@@ -144,4 +145,73 @@ class WasmTests(unittest.TestCase):
         )
         assert (
             mock_client.last_request == "/wasm/v1beta1/contract/fetchcontractaddress?"
+        )
+
+    def test_query_contract_by_code(self):
+        content = {
+            "contracts": ["fetch18vd8fpwxzck93qlwghaj6arh4p7c5n890l3amr"],
+            "pagination": {"total": "1"},
+        }
+
+        expected_response = ParseDict(content, QueryContractsByCodeResponse())
+
+        mock_client = MockQueryRestClient(json.dumps(content))
+        wasm = WasmRestClient(mock_client)
+
+        assert (
+            wasm.ContractsByCode(QueryContractsByCodeRequest(code_id=1))
+            == expected_response
+        )
+        assert mock_client.last_request == "/wasm/v1beta1/code/1/contracts?"
+
+    def test_query_contract_history(self):
+        content = {
+            "contracts": ["fetch18vd8fpwxzck93qlwghaj6arh4p7c5n890l3amr"],
+            "pagination": {"total": "1"},
+        }
+
+        expected_response = ParseDict(content, QueryContractsByCodeResponse())
+
+        mock_client = MockQueryRestClient(json.dumps(content))
+        wasm = WasmRestClient(mock_client)
+
+        assert (
+            wasm.ContractsByCode(QueryContractsByCodeRequest(code_id=1))
+            == expected_response
+        )
+        assert mock_client.last_request == "/wasm/v1beta1/code/1/contracts?"
+
+    def test_query_contract_history(self):
+        msg = {}
+        base64_msg = base64.b64encode(json.dumps(msg).encode("UTF8")).decode()
+
+        content = {
+            "entries": [
+                {
+                    "operation": "CONTRACT_CODE_HISTORY_OPERATION_TYPE_INIT",
+                    "code_id": "2",
+                    "msg": base64_msg,
+                }
+            ],
+            "pagination": {"total": "1"},
+        }
+
+        expected_response = ParseDict(content, QueryContractHistoryResponse())
+
+        # Replace base64 msg with original dict which would be returned by REST api to generate get response
+        content["entries"][0]["msg"] = msg
+        raw_content = json.dumps(content)
+        mock_client = MockQueryRestClient(raw_content)
+
+        wasm = WasmRestClient(mock_client)
+
+        assert (
+            wasm.ContractHistory(
+                QueryContractHistoryRequest(address="fetchcontractaddress")
+            )
+            == expected_response
+        )
+        assert (
+            mock_client.last_request
+            == "/wasm/v1beta1/contract/fetchcontractaddress/history?"
         )
