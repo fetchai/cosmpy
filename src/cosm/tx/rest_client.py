1d2f25eab7d8666a841459e6f1c601046922b15f
jiri <jiri.vestfal@fetch.ai>
2021-08-05 10:36:43 +0100

    rest_client docstrings

--- src/cosm/tx/rest_client.py
+++ src/cosm/tx/rest_client.py
@@ -1,6 +1,8 @@
 import json
 import base64
 
+from common import JSONLike
+from typing import List
 from cosm.query.rest_client import QueryRestClient as RestClient
 from cosm.tx.interface import RPCInterface
 
@@ -22,16 +24,31 @@ class TxRestClient(RPCInterface):
     txs_url_path = "/cosmos/tx/v1beta1/txs"
 
     def __init__(self, rest_client: RestClient):
+        """
+        Tx REST client
+
+        :param rest_client: REST api client
+        """
         self.rest_client = rest_client
 
     def Simulate(self, request: SimulateRequest) -> SimulateResponse:
-        """Simulate simulates executing a transaction for estimating gas usage."""
+        """
+        Simulate simulates executing a transaction for estimating gas usage.
+
+        :param request: SimulateRequest
+        :return: SimulateResponse
+        """
         json_request = MessageToDict(request)
         response = self.rest_client.post("/cosmos/tx/v1beta1/simulate", json_request)
         return Parse(response, SimulateResponse())
 
     def GetTx(self, request: GetTxRequest) -> GetTxResponse:
-        """GetTx fetches a tx by hash."""
+        """
+        GetTx fetches a tx by hash.
+
+        :param request: GetTxRequest
+        :return: GetTxResponse
+        """
         json_request = MessageToDict(request)
         json_request.pop("hash")
         url_encoded_request = urlencode(json_request)
@@ -47,20 +64,35 @@ class TxRestClient(RPCInterface):
         return ParseDict(dict_response, GetTxResponse())
 
     def BroadcastTx(self, request: BroadcastTxRequest) -> BroadcastTxResponse:
-        """BroadcastTx broadcast transaction."""
+        """
+        BroadcastTx broadcast transaction.
+
+        :param request: BroadcastTxRequest
+        :return: BroadcastTxResponse
+        """
         json_request = MessageToDict(request)
         response = self.rest_client.post(self.txs_url_path, json_request)
         return Parse(response, BroadcastTxResponse())
 
     def GetTxsEvent(self, request: GetTxsEventRequest) -> GetTxsEventResponse:
-        """GetTxsEvent fetches txs by event."""
+        """
+        GetTxsEvent fetches txs by event.
+
+        :param request: GetTxsEventRequest
+        :return: GetTxsEventResponse
+        """
         json_request = MessageToDict(request)
         url_encoded_request = urlencode(json_request)
         response = self.rest_client.get(f"{self.txs_url_path}&{url_encoded_request}")
         return Parse(response, GetTxsEventResponse())
 
     @staticmethod
-    def _fix_messages(messages):
+    def _fix_messages(messages: List[JSONLike]):
+        """
+        Fix for REST api response in case of CosmWasm messages contains dict instead of base64 encoded string
+
+        :param messages: List of message in Tx response
+        """
         for message in messages:
             if message["@type"] == "/cosmwasm.wasm.v1beta1.MsgInstantiateContract":
                 message["init_msg"] = base64.b64encode(
