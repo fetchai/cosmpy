5adf887e0d5b5611c050ac1ad654d853bd7c37df
jiri <jiri.vestfal@fetch.ai>
2021-08-06 08:21:59 +0100

    check fixes

--- src/cosm/clients/cosmwasm_client.py
+++ src/cosm/clients/cosmwasm_client.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2021 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Implementation of CosmWasm query client."""
+
 import json
 from typing import Union
 
@@ -19,6 +40,8 @@ from cosmwasm.wasm.v1beta1.query_pb2_grpc import QueryStub as CosmWasmGrpcClient
 
 
 class CosmWasmClient:
+    """High level client for REST/gRPC node interaction."""
+
     def __init__(self, channel: Union[Channel, QueryRestClient]):
         """
         :param channel: gRPC or REST querying client
