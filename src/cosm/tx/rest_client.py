194d248b9493bd9c2e2cdb720fa3d4e14499a7a8
Jiri Vestfal <45360788+MissingNO57@users.noreply.github.com>
2021-08-06 12:11:22 +0200

    High level structures - CosmWasmClient and SigningCosmWasmClient (#27)
    High level structures - CosmWasmClient and SigningCosmWasmClient

--- src/cosm/tx/rest_client.py
+++ src/cosm/tx/rest_client.py
@@ -19,10 +19,14 @@
 
 """Implementation of Tx interface using REST."""
 
+import base64
+import json
+from typing import List
 from urllib.parse import urlencode
 
-from google.protobuf.json_format import MessageToDict, Parse
+from google.protobuf.json_format import MessageToDict, Parse, ParseDict
 
+from common import JSONLike
 from cosm.query.rest_client import QueryRestClient as RestClient
 from cosm.tx.interface import TxInterface
 from cosmos.tx.v1beta1.service_pb2 import (
@@ -51,29 +55,73 @@ class TxRestClient(TxInterface):
         self.rest_client = rest_client
 
     def Simulate(self, request: SimulateRequest) -> SimulateResponse:
-        """Simulate simulates executing a transaction for estimating gas usage."""
+        """
+        Simulate executing a transaction to estimate gas usage.
+
+        :param request: SimulateRequest
+        :return: SimulateResponse
+        """
         json_request = MessageToDict(request)
         response = self.rest_client.post("/cosmos/tx/v1beta1/simulate", json_request)
         return Parse(response, SimulateResponse())
 
     def GetTx(self, request: GetTxRequest) -> GetTxResponse:
-        """GetTx fetches a tx by hash."""
+        """
+        GetTx fetches a tx by hash.
+
+        :param request: GetTxRequest
+        :return: GetTxResponse
+        """
         json_request = MessageToDict(request)
+        json_request.pop("hash")
         url_encoded_request = urlencode(json_request)
         response = self.rest_client.get(
-            f"{self.txs_url_path}&{url_encoded_request}",
+            f"{self.txs_url_path}/{request.hash}?{url_encoded_request}",
         )
-        return Parse(response, GetTxResponse())
+
+        # JSON in JSON in case of CosmWasm messages workaround
+        dict_response = json.loads(response)
+        self._fix_messages(dict_response["tx"]["body"]["messages"])
+        self._fix_messages(dict_response["tx_response"]["tx"]["body"]["messages"])
+
+        return ParseDict(dict_response, GetTxResponse())
 
     def BroadcastTx(self, request: BroadcastTxRequest) -> BroadcastTxResponse:
-        """BroadcastTx broadcast transaction."""
+        """
+        BroadcastTx broadcast transaction.
+
+        :param request: BroadcastTxRequest
+        :return: BroadcastTxResponse
+        """
         json_request = MessageToDict(request)
         response = self.rest_client.post(self.txs_url_path, json_request)
         return Parse(response, BroadcastTxResponse())
 
     def GetTxsEvent(self, request: GetTxsEventRequest) -> GetTxsEventResponse:
-        """GetTxsEvent fetches txs by event."""
+        """
+        GetTxsEvent fetches txs by event.
+
+        :param request: GetTxsEventRequest
+        :return: GetTxsEventResponse
+        """
         json_request = MessageToDict(request)
         url_encoded_request = urlencode(json_request)
         response = self.rest_client.get(f"{self.txs_url_path}&{url_encoded_request}")
         return Parse(response, GetTxsEventResponse())
+
+    @staticmethod
+    def _fix_messages(messages: List[JSONLike]):
+        """
+        Fix for REST api response in case of CosmWasm messages contains dict instead of base64 encoded string
+
+        :param messages: List of message in Tx response
+        """
+        for message in messages:
+            if message["@type"] == "/cosmwasm.wasm.v1beta1.MsgInstantiateContract":
+                message["init_msg"] = base64.b64encode(
+                    json.dumps(message["init_msg"]).encode("UTF8")
+                ).decode()
+            if message["@type"] == "/cosmwasm.wasm.v1beta1.MsgExecuteContract":
+                message["msg"] = base64.b64encode(
+                    json.dumps(message["msg"]).encode("UTF8")
+                ).decode()
---
+++ src/examples/tx_native_tokens_atomic_swap_rest_example.py
@@ -0,0 +1,77 @@
+""" REST example of native tokens atomic swap """
+
+from cosm.crypto.keypairs import PrivateKey
+from cosmos.base.v1beta1.coin_pb2 import Coin
+
+from cosm.clients.signing_cosmwasm_client import SigningCosmWasmClient
+from cosm.query.rest_client import QueryRestClient
+
+# Denomination and amount of transferred tokens
+DENOM_1 = "stake"
+AMOUNT_1 = [Coin(amount="1", denom=DENOM_1)]
+DENOM_2 = "atestfet"
+AMOUNT_2 = [Coin(amount="1", denom=DENOM_2)]
+
+# Node config
+REST_ENDPOINT_ADDRESS = "http://localhost:1317"
+CHAIN_ID = "testing"
+
+# Private key of validator's account
+VALIDATOR_PK = PrivateKey(
+    bytes.fromhex(
+        "0ba1db680226f19d4a2ea64a1c0ea40d1ffa3cb98532a9fa366994bb689a34ae"
+    )
+)
+
+# Private key of bob's account
+BOB_PK = PrivateKey(
+    bytes.fromhex(
+        "439861b21d146e83fe99496f4998a305c83cfbc24717c77e32b06d224bf1e636"
+    )
+)
+
+# Create clients
+channel = QueryRestClient(REST_ENDPOINT_ADDRESS)
+validator_client = SigningCosmWasmClient(VALIDATOR_PK, channel, CHAIN_ID)
+bob_client = SigningCosmWasmClient(BOB_PK, channel, CHAIN_ID)
+
+# Print balances before transfer
+print("Before transaction")
+denom_1_balance = validator_client.get_balance(validator_client.address, DENOM_1)
+denom_2_balance = validator_client.get_balance(validator_client.address, DENOM_2)
+print(f"Validator has {denom_1_balance.balance.amount} {DENOM_1} and {denom_2_balance.balance.amount} {DENOM_2}")
+denom_1_balance = bob_client.get_balance(bob_client.address, DENOM_1)
+denom_2_balance = bob_client.get_balance(bob_client.address, DENOM_2)
+print(f"Bob has {denom_1_balance.balance.amount} {DENOM_1} and {denom_2_balance.balance.amount} {DENOM_2}")
+
+# Create atomic-swap send messages
+# Transfer AMOUNT_1 from validator to bob
+msg_1 = validator_client.get_packed_send_msg(from_address=validator_client.address,
+                                             to_address=bob_client.address,
+                                             amount=AMOUNT_1)
+
+# Transfer AMOUNT_2 from bob to validator
+msg_2 = bob_client.get_packed_send_msg(from_address=bob_client.address,
+                                       to_address=validator_client.address,
+                                       amount=AMOUNT_2)
+
+# Generate one transaction containing both messages
+tx = validator_client.generate_tx(packed_msgs=[msg_1, msg_2],
+                                  from_addresses=[validator_client.address, bob_client.address],
+                                  pub_keys=[validator_client.public_key_bytes, bob_client.public_key_bytes])
+
+# Sign transaction by both clients
+validator_client.sign_tx(tx)
+bob_client.sign_tx(tx)
+
+# Broadcast transaction
+validator_client.broadcast_tx(tx)
+
+# Print balances after transfer
+print("After transaction")
+denom_1_balance = validator_client.get_balance(validator_client.address, DENOM_1)
+denom_2_balance = validator_client.get_balance(validator_client.address, DENOM_2)
+print(f"Validator has {denom_1_balance.balance.amount} {DENOM_1} and {denom_2_balance.balance.amount} {DENOM_2}")
+denom_1_balance = bob_client.get_balance(bob_client.address, DENOM_1)
+denom_2_balance = bob_client.get_balance(bob_client.address, DENOM_2)
+print(f"Bob has {denom_1_balance.balance.amount} {DENOM_1} and {denom_2_balance.balance.amount} {DENOM_2}")
