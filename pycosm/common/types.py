9af6624cc8743f42e3a01355d6af9be9c6a6a5d5
jiri <jiri.vestfal@fetch.ai>
2021-08-17 15:06:11 +0100

    comment

--- pycosm/common/types.py
+++ pycosm/common/types.py
@@ -25,4 +25,5 @@ Primitive = Union[str, int, bool, float]
 _JSONDict = Dict[Any, Any]  # temporary placeholder
 _JSONList = List[Any]  # temporary placeholder
 _JSONType = Optional[Union[Primitive, _JSONDict, _JSONList]]
+# Added Dict[str, _JSONDict] as workaround to not properly resolving recursive types - _JSONDict should be subset of _JSONType
 JSONLike = Union[Dict[str, _JSONType], Dict[str, _JSONDict]]
