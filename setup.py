f60699102263f5bd9e4c7fda125f93282b9bf20b
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2021-08-23 16:52:14 +0100

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="cosmpy",
-    version="0.1.1",
+    version="0.1.2",
     description="A library for interacting with the cosmos networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
