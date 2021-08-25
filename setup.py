cdda2f7d3f590b7db658e0b483b7e5688e3eee9c
jiri <jiri.vestfal@fetch.ai>
2021-08-25 10:30:45 +0100

    gprcio-tools removed

--- setup.py
+++ setup.py
@@ -59,8 +59,7 @@ setup(
         "google-api-python-client",
         "protobuf",
         # Using Tensorflow v2.4.0 was causing conflicts because it requires grpcio==1.32.0
-        "grpcio==1.32.0",
-        "grpcio-tools==1.32.0",
+        "grpcio>=1.32.0",
     ],
     extras_require={
         "dev": ["check-manifest", "flake8", "black", "mypy"],
