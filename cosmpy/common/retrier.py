dc642c4f1519a010d7add87900cc0452196dc3cb
Jiri Vestfal <jiri.vestfal@fetch.ai>
2022-02-03 11:53:57 +0000

    fixes

--- cosmpy/common/retrier.py
+++ cosmpy/common/retrier.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2021 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Implementation of Retrier."""
+
+
 import time
 from typing import Callable, Type
 
@@ -8,7 +30,7 @@ _logger = get_logger(__name__)
 
 class Retrier:
     """
-    Implementation of a ledger service class.
+    Class for exception handling with retries
     """
 
     def __init__(
