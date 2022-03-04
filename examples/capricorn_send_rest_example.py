9393ad436366401f6e275cdded5d4fa1c900d65b
Jiri Vestfal <jiri.vestfal@fetch.ai>
2022-03-04 12:17:50 +0000

    Copyright info

--- examples/capricorn_send_rest_example.py
+++ examples/capricorn_send_rest_example.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2021 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+""" REST example of sending and querying funds using Capricorn testnet """
+
 from cosmpy.clients.crypto import CosmosCrypto
 from cosmpy.clients.ledger import CosmosLedger
 from cosmpy.protos.cosmos.base.v1beta1.coin_pb2 import Coin
