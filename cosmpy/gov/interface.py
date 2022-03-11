4edc01bfb5f5165c33a7a4c0f89f55c5da3d657d
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2022-03-11 16:17:21 +0300

    docstrings improved darglint check added to CI workflow

--- cosmpy/gov/interface.py
+++ cosmpy/gov/interface.py
@@ -45,32 +45,79 @@ class Gov(ABC):
 
     @abstractmethod
     def Proposal(self, request: QueryProposalRequest) -> QueryProposalResponse:
-        """Proposal queries proposal details based on ProposalID."""
+        """
+        Proposal queries proposal details based on ProposalID.
+
+        :param request: QueryProposalRequest with proposal id
+
+        :return: QueryProposalResponse
+        """
 
     @abstractmethod
     def Proposals(self, request: QueryProposalsRequest) -> QueryProposalsResponse:
-        """Proposals queries all proposals based on given status."""
+        """
+        Proposals queries all proposals based on given status.
+
+        :param request: QueryProposalsRequest
+
+        :return: QueryProposalsResponse
+        """
 
     @abstractmethod
     def Vote(self, request: QueryVoteRequest) -> QueryVoteResponse:
-        """Vote queries voted information based on proposalID, voterAddr."""
+        """
+        Vote queries voted information based on proposalID, voterAddr.
+
+        :param request: QueryVoteRequest with voter and proposal id
+
+        :return: QueryVoteResponse
+        """
 
     @abstractmethod
     def Votes(self, request: QueryVotesRequest) -> QueryVotesResponse:
-        """Votes queries votes of a given proposal."""
+        """
+        Votes queries votes of a given proposal.
+
+        :param request: QueryVotesResponse with proposal id
+
+        :return: QueryVotesResponse
+        """
 
     @abstractmethod
     def Params(self, request: QueryParamsRequest) -> QueryParamsResponse:
-        """Params queries all parameters of the gov module."""
+        """
+        Params queries all parameters of the gov module.
+
+        :param request: QueryParamsRequest with params_type
+
+        :return: QueryParamsResponse
+        """
 
     @abstractmethod
     def Deposit(self, request: QueryDepositRequest) -> QueryDepositResponse:
-        """Deposit queries single deposit information based proposalID, depositAddr."""
+        """
+        Deposit queries single deposit information based proposalID, depositAddr.
+
+        :param request: QueryDepositRequest with depositor and proposal_id
+
+        :return: QueryDepositResponse
+        """
 
     @abstractmethod
     def Deposits(self, request: QueryDepositsRequest) -> QueryDepositsResponse:
-        """Deposits queries all deposits of a single proposal."""
+        """Deposits queries all deposits of a single proposal.
+
+        :param request: QueryDepositsRequest with proposal_id
+
+        :return: QueryDepositsResponse
+        """
 
     @abstractmethod
     def TallyResult(self, request: QueryTallyResultRequest) -> QueryTallyResultResponse:
-        """Tally Result queries the tally of a proposal vote."""
+        """
+        Tally Result queries the tally of a proposal vote.
+
+        :param request: QueryTallyResultRequest with proposal_id
+
+        :return: QueryTallyResultResponse
+        """
--- cosmpy/gov/rest_client.py
+++ cosmpy/gov/rest_client.py
@@ -56,14 +56,27 @@ class GovRestClient(Gov):
         self._rest_api = rest_api
 
     def Proposal(self, request: QueryProposalRequest) -> QueryProposalResponse:
-        """Proposal queries proposal details based on ProposalID."""
+        """
+        Proposal queries proposal details based on ProposalID.
+
+        :param request: QueryProposalRequest with proposal id
+
+        :return: QueryProposalResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}",
         )
         return Parse(json_response, QueryProposalResponse())
 
     def Proposals(self, request: QueryProposalsRequest) -> QueryProposalsResponse:
-        """Proposals queries all proposals based on given status."""
+        """
+        Proposals queries all proposals based on given status.
+
+        :param request: QueryProposalsRequest
+
+        :return: QueryProposalsResponse
+        """
+
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/",
             request,
@@ -71,14 +84,26 @@ class GovRestClient(Gov):
         return Parse(json_response, QueryProposalsResponse())
 
     def Vote(self, request: QueryVoteRequest) -> QueryVoteResponse:
-        """Vote queries voted information based on proposalID, voterAddr."""
+        """
+        Vote queries voted information based on proposalID, voterAddr.
+
+        :param request: QueryVoteRequest with voter and proposal id
+
+        :return: QueryVoteResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}/votes/{request.voter}"
         )
         return Parse(json_response, QueryVoteResponse())
 
     def Votes(self, request: QueryVotesRequest) -> QueryVotesResponse:
-        """Votes queries votes of a given proposal."""
+        """
+        Votes queries votes of a given proposal.
+
+        :param request: QueryVotesResponse with proposal id
+
+        :return: QueryVotesResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}/votes/",
             request,
@@ -87,21 +112,39 @@ class GovRestClient(Gov):
         return Parse(json_response, QueryVotesResponse())
 
     def Params(self, request: QueryParamsRequest) -> QueryParamsResponse:
-        """Params queries all parameters of the gov module."""
+        """
+        Params queries all parameters of the gov module.
+
+        :param request: QueryParamsRequest with params_type
+
+        :return: QueryParamsResponse
+        """
+
         json_response = self._rest_api.get(
             f"{self.API_URL}/params/{request.params_type}"
         )
         return Parse(json_response, QueryParamsResponse())
 
     def Deposit(self, request: QueryDepositRequest) -> QueryDepositResponse:
-        """Deposit queries single deposit information based proposalID, depositAddr."""
+        """
+        Deposit queries single deposit information based proposalID, depositAddr.
+
+        :param request: QueryDepositRequest with depositor and proposal_id
+
+        :return: QueryDepositResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}/deposits/{request.depositor}"
         )
         return Parse(json_response, QueryDepositResponse())
 
     def Deposits(self, request: QueryDepositsRequest) -> QueryDepositsResponse:
-        """Deposits queries all deposits of a single proposal."""
+        """Deposits queries all deposits of a single proposal.
+
+        :param request: QueryDepositsRequest with proposal_id
+
+        :return: QueryDepositsResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}/deposits/",
             request,
@@ -110,7 +153,13 @@ class GovRestClient(Gov):
         return Parse(json_response, QueryDepositsResponse())
 
     def TallyResult(self, request: QueryTallyResultRequest) -> QueryTallyResultResponse:
-        """Tally Result queries the tally of a proposal vote."""
+        """
+        Tally Result queries the tally of a proposal vote.
+
+        :param request: QueryTallyResultRequest with proposal_id
+
+        :return: QueryTallyResultResponse
+        """
         json_response = self._rest_api.get(
             f"{self.API_URL}/proposals/{request.proposal_id}/tally"
         )
