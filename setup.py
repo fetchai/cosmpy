ce7e5092bd68d91f090cf957194bb661a1dcf1a1
VijaySharma815 <vijaysharmadev87@gmail.com>
2022-08-01 12:40:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -83,8 +83,6 @@ setup(
             "pylint==2.9.6",
             "liccheck==0.6.2",
             "flake8-copyright==0.2.2",
-            # Using Tensorflow v2.4.0 was causing conflicts because it requires grpcio==1.32.0
-            "grpcio-tools<=1.32.0",
             "flake8-bugbear==20.1.4",
             "flake8-eradicate==0.4.0",
             "flake8-docstrings==1.6.0",
