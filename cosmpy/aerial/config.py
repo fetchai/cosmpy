b771a64a75b7fa1a0e15e64cc83d06c79667483e
VijaySharma815 <vijaysharmadev87@gmail.com>
2022-08-26 09:10:33 +0530

    Linting fixes

--- cosmpy/aerial/config.py
+++ cosmpy/aerial/config.py
@@ -66,8 +66,13 @@ class NetworkConfig:
         if self.url == "":
             raise NetworkConfigError("URL must be set")
         if not any(
-            map(lambda x: self.url.startswith(x), URL_PREFIXES)
-        ):  # pylint: disable=unnecessary-lambda
+            map(
+                lambda x: self.url.startswith(  # noqa: # pylint: disable=unnecessary-lambda
+                    x
+                ),
+                URL_PREFIXES,
+            )
+        ):
             prefix_list = ", ".join(map(lambda x: f'"{x}"', URL_PREFIXES))
             raise NetworkConfigError(
                 f"URL must start with one of the following prefixes: {prefix_list}"
