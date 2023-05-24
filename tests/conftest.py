26bfb2e560797a3e4c7b77db08af8d65ebf378fa
Jiri Vestfal <jiri.vestfal@fetch.ai>
2023-05-24 16:21:04 +0200

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,35 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2021 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Conftest module."""
+
+from typing import List
+
 import pytest
 
 
-def pytest_collection_modifyitems(_, items):
+def pytest_collection_modifyitems(_, items: List[pytest.Item]):
+    """
+    Modify items collection for pytest.
+
+    :param items: list of items
+    """
     # Remove third party tests from integration tests
     for item in items:
         if "third_party" in item.nodeid:
